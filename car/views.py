32d4ca7b2c0d954cb23700bbaf562f87b66dc7ea
Mohsen Hafezi <mohi.developer91@gmail.com>
2024-07-27 20:02:37 +0330

    some new code for getting data from database in views.py

--- car/views.py
+++ car/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car
 
 
 def car_page(request):
-    return HttpResponse("This is the main page of the car apps.")
+    cars = Car.objects.all()  # It will get all cars
+    cars2 = Car.objects.get(model='Cerato')
+    return HttpResponse(cars2.color)
