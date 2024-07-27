c5aa1e9567c547c2cd885c3fbe4da16bc61b616a
Mohsen Hafezi <mohi.developer91@gmail.com>
2024-07-27 09:57:21 +0330

    add Person class in models.py and add models in admin panel.

--- car/models.py
+++ car/models.py
@@ -9,8 +9,18 @@ class Car(models.Model):
     color = models.CharField(max_length=50)
     year = models.PositiveIntegerField(validators=[validate_year])
     kilometers = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
     city = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.brand} {self.model} ({self.year})"
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
