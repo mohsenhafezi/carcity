263a67857cae7f9785179da89e0b7dbe6ffe9e14
Mohsen Hafezi <mohi.developer91@gmail.com>
2024-07-28 19:18:53 +0330

    add relationship to the models.py

--- car/models.py
+++ car/models.py
@@ -2,21 +2,6 @@ from django.db import models
 from .validators import validate_year
 
 
-# these classes are related to the Car app in this site
-class Car(models.Model):
-    brand = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-    created_date = models.DateTimeField(auto_now_add=True)
-    color = models.CharField(max_length=50)
-    year = models.PositiveIntegerField(validators=[validate_year])
-    kilometers = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=20, decimal_places=2)
-    city = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.brand} {self.model} ({self.year})"
-
-
 class Person(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -25,3 +10,26 @@ class Person(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+# these classes are related to the Car app in this Website
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    # this auto now adds feature will add date time automatically to the field
+    # if we have an update record, it should have an "auto_now = True"
+    color = models.CharField(max_length=50)
+    year = models.PositiveIntegerField(validators=[validate_year])
+    kilometers = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    city = models.CharField(max_length=100)
+    registered_by = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    # if Person class defined after the car model, then we can introduce Person as string to avoid an error:
+    # registered_by = models.ForeignKey("Person", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
