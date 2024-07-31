d6b0507a0e276f7febc411789136e2852b0e4758
Mohsen Hafezi <mohi.developer91@gmail.com>
2024-07-31 12:52:12 +0330

    Add another folder to the templates folder to clarify.

--- car/views.py
+++ car/views.py
@@ -7,4 +7,4 @@ def car_page(request):
     # cars = Car.objects.all()  # It will get all cars
     # cars2 = Car.objects.get(model='Cerato')
     # return HttpResponse(cars2.color)
-    return render(request, 'index.html')
+    return render(request, 'car/car_list.html')
