b86345603e58c00f34b325c2352f42861a750571
Mohsen Hafezi <mohi.developer91@gmail.com>
2024-08-01 17:31:19 +0330

    add teamplate tag url and name property to the project - add app_name

--- car/urls.py
+++ car/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import car_page
 
 # all paths of car app should add here to apply after "/car" path
+app_name = 'car'
 urlpatterns = [
     path('', car_page, name='car'),
 ]
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home_page
 
+app_name = 'home'
 urlpatterns = [
     path('', home_page),
 ]
