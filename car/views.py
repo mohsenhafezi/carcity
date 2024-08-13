62d7bc72c0532f24d4f83721adaaa08a96bc439e
Mohsen Hafezi <mohi.developer91@gmail.com>
2024-08-13 12:24:12 +0330

    improve search method in views.py

--- car/views.py
+++ car/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Car
@@ -5,9 +6,6 @@ from .models import Car
 
 def car_page(request):
     cars = Car.objects.filter(is_show=True).order_by('created_date')  # It will get all cars
-    # search_keyword = request.GET.get('Search')
-    # if search_keyword:
-    #     cars = cars.filter(model__icontains=search_keyword).order_by('created_date')
     context = {'cars': cars}
     return render(request, 'car/car_list.html', context)
 
@@ -19,14 +17,14 @@ def car_detail(request, car_id):
 
 
 def car_search(request):
-    # cars = Car.objects.filter(is_show=True).order_by('-id')[:2]  # It will get all cars
     search_keyword = request.GET.get('Search')
     if search_keyword:
-        cars = Car.objects.filter(is_show=True, model__icontains=search_keyword)
+        cars = Car.objects.filter(Q(is_show=True) &
+                                  (Q(model__icontains=search_keyword) |
+                                   Q(brand__icontains=search_keyword) |
+                                   Q(year__icontains=search_keyword)
+                                   ))
     else:
-        # در صورت عدم وجود جستجو، دو مورد آخر را برگردانید
         cars = Car.objects.filter(is_show=True).order_by('-id')[:2]
-    # if search_keyword:
-    #     cars = cars.filter(model__icontains=search_keyword)
     context = {'cars': cars}
     return render(request, 'car/car_search.html', context)
